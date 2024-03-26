2b50cca807af31d551eb1d1ea4297635c0f54aa2
Jay Barrett <jab864@cornell.edu>
2024-03-26 19:59:04 -0400

    Fixed bug in Auth.ml that made it harder to use tests
    - Make sure when you want to run a test
    you set the test env
    $ export ENV=test
    - When you're done unset this to test main.ml
    $ unset ENV
    
    - When in doubt reconsult the TEST_README.txt
    - I have it there to make sure this isn't an issue
    - This is because it changes which .csv is logged
    for registering and determining login.
    
    Thanks

---
+++ TEST_README.txt
@@ -0,0 +1,8 @@
+When you want to run tests in general,
+make sure you put in terminal
+$ export ENV=test
+
+When you're done, make sure you then do
+$ unset ENV
+
+This is essential for making sure it exports to the right csvs. 
--- lib/auth.ml
+++ lib/auth.ml
@@ -3,12 +3,17 @@ open Cryptokit
 (* Cite ChatGPT for cryptokit password hashing *)
 
 let credentials_path () =
-  match Sys.getenv_opt "CREDENTIALS_PATH" with
-  | Some path -> path
-  | None -> (
-      match Sys.getenv_opt "ENV" with
-      | Some "test" -> "test/test_auth.csv"
-      | _ -> "data/user_credentials.csv")
+  let path =
+    match Sys.getenv_opt "CREDENTIALS_PATH" with
+    | Some path -> path
+    | None -> (
+        match Sys.getenv_opt "ENV" with
+        | Some "test" -> "test/test_auth.csv"
+        | _ -> "data/user_credentials.csv")
+  in
+  print_endline ("Using credentials path: " ^ path);
+  (* Debug print *)
+  path
 
 let username_exists username =
   let credentials = Csv.load (credentials_path ()) in
@@ -17,12 +22,21 @@ let username_exists username =
 let hash_password password = hash_string (Hash.sha256 ()) password
 
 let add_user username hashed_password =
-  if username_exists username then false
-  else
-    let credentials = Csv.load (credentials_path ()) in
-    Csv.save (credentials_path ())
-      (credentials @ [ [ username; hashed_password ] ]);
-    true
+  try
+    if username_exists username then false
+    else
+      let credentials = Csv.load (credentials_path ()) in
+      Csv.save (credentials_path ())
+        (credentials @ [ [ username; hashed_password ] ]);
+      true
+  with
+  | Sys_error msg ->
+      Printf.eprintf "File system error: %s\n" msg;
+      false
+  | e ->
+      Printf.eprintf "CSV operation failed or unexpected exception: %s\n"
+        (Printexc.to_string e);
+      false
 
 let authenticate username password =
   let credentials = Csv.load (credentials_path ()) in
