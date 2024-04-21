de8165376c5bc1225d3af0d96761193df8cc43ea
ashalis <abs325@cornell.edu>
2024-04-21 17:17:19 -0400

    Reworked financial.ml, altered overview, and added financial_stock.ml

--- lib/overview.ml
+++ lib/overview.ml
@@ -1,7 +1,8 @@
 (* overview.ml *)
 open Data
 open ANSITerminal
-open Lwt
+
+(* open Lwt *)
 open Financial
 
 let print_strings style lines =
@@ -116,7 +117,7 @@ and process_choice user =
   match choice with
   | "1" -> mood_interface user
   | "2" -> health_interface user
-  (* | "3" -> financial_interface user *)
+  | "3" -> financial_interface user
   | "4" -> ()
   | "5" ->
       print_endline "Exiting...";
@@ -139,51 +140,54 @@ and dashboard_login user =
 
 (* financial interface *)
 
-(* and financial_interface user =
-   print_string [ Reset; Bold; Foreground Green ] "\nFinancial Tracker\n";
-   print_strings [ Reset ]
-     [
-       "1. View personal stock spread\n";
-       "2. Manage stock options\n";
-       "3. View all bank accounts\n";
-       "4. Add bank to overall wallet\n";
-       "5. Edit funds in bank wallet\n";
-       "6. Return to main menu\n";
-     ];
-   print_string [ Bold ] "Please enter a command: ";
-   financial_input user *)
+and financial_interface user =
+  print_string [ Reset; Bold; Foreground Green ] "\nFinancial Tracker\n";
+  print_strings [ Reset ]
+    [
+      "1. View personal stock spread\n";
+      "2. View all bank accounts\n";
+      "3. Add bank to overall wallet\n";
+      "4. Edit funds in bank wallet\n";
+      "5. Return to main menu\n";
+    ];
+  print_string [ Bold ] "Please enter a command: ";
+  financial_input user
 
-(* and financial_input user =
-   Lwt_io.read_line Lwt_io.stdin >>= fun choice ->
-   match choice with
-   | "1" -> view_stock_spread user >>= fun () -> financial_interface user
-   | "2" -> manage_stock_options user >>= fun () -> financial_interface user
-   | "3" -> view_all_banks user >>= fun () -> financial_interface user
-   | "4" -> prompt_add_wallet user >>= fun () -> financial_interface user
-   | "5" -> prompt_edit_wallet user >>= fun () -> financial_interface user
-   | "6" -> dashboard_login user >>= fun () -> Lwt.return_unit
-   | _ ->
-       Lwt_io.printl "Invalid option. Please try again." >>= fun () ->
-       financial_interface user;
-       Lwt.return_unit *)
+and financial_input user =
+  let choice = read_line () in
+  match choice with
+  (* | "1" -> view_stock_spread user >>= fun () -> financial_interface user
+     | "2" -> manage_stock_options user >>= fun () -> financial_interface user *)
+  | "2" ->
+      view_all_banks user;
+      financial_interface user
+  | "3" ->
+      prompt_add_wallet user;
+      financial_interface user
+  | "4" ->
+      prompt_edit_wallet user;
+      financial_interface user
+  | "5" -> dashboard_login user
+  | _ ->
+      print_endline "Invalid option. Please try again.";
+      financial_interface user
 
-and view_all_banks user = view_wallet_spread user >>= fun () -> Lwt.return ()
+and view_all_banks user = view_wallet_spread user
 
 and prompt_add_wallet user =
-  Lwt_io.printl "Enter wallet name: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun wallet_name ->
-  Lwt_io.printl "Enter initial balance: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun balance ->
-  add_wallet user wallet_name (float_of_string balance) >>= fun () ->
-  Lwt.return ()
+  print_endline "Enter wallet name: ";
+  let wallet_name = read_line () in
+  let () = print_endline "Enter initial balance: " in
+  let balance = read_line () in
+  add_wallet user wallet_name (float_of_string balance)
 
 and prompt_edit_wallet user =
-  Lwt_io.printl "Enter wallet name to edit: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun wallet_name ->
-  Lwt_io.printl "Select operation (add/subtract/set): " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun operation ->
-  Lwt_io.printl "Enter amount: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun amount ->
+  let () = print_endline "Enter wallet name to edit: " in
+  let wallet_name = read_line () in
+  let () = print_endline "Select operation (add/subtract/set): " in
+  let operation = read_line () in
+  let () = print_endline "Enter amount: " in
+  let amount = read_line () in
   let op =
     match operation with
     | "add" -> Add
@@ -192,57 +196,69 @@ and prompt_edit_wallet user =
     | _ -> failwith "Invalid operation"
   in
   adjust_wallet_balance user wallet_name op (float_of_string amount)
-  >>= fun () -> Lwt.return ()
 
-and manage_stock_options user =
-  print_string [ Reset; Bold; Foreground Blue ] "\nStock Management\n";
+and stock_interface user =
+  print_string [ Reset; Bold; Foreground Green ] "\nStock Tracker\n";
   print_strings [ Reset ]
     [
-      "1. Add a new stock\n";
-      "2. Remove a stock\n";
-      "3. Modify a stock\n";
-      "4. Update stock prices\n";
-      "5. Return to financial menu\n";
+      "1. View personal stock spread\n";
+      "2. View all bank accounts\n";
+      "3. Add bank to overall wallet\n";
+      "4. Edit funds in bank wallet\n";
+      "5. Return to main menu\n";
     ];
-  print_string [ Bold ] "Select an option: ";
-  stock_input user
+  print_string [ Bold ] "Please enter a command: ";
+  financial_input user
 
-and stock_input user =
-  let choice = read_line () in
-  match choice with
-  | "1" -> prompt_add_stock user >>= fun () -> manage_stock_options user
-  | "2" -> prompt_remove_stock user >>= fun () -> manage_stock_options user
-  | "3" -> prompt_modify_stock user >>= fun () -> manage_stock_options user
-  | "4" -> update_stock_prices user >>= fun () -> manage_stock_options user
-  (* | "5" -> financial_interface user *)
-  | _ ->
-      Lwt_io.printl "Invalid choice. Please try again." >>= fun () ->
-      manage_stock_options user
+(* and manage_stock_options user =
+     print_string [ Reset; Bold; Foreground Blue ] "\nStock Management\n";
+     print_strings [ Reset ]
+       [
+         "1. Add a new stock\n";
+         "2. Remove a stock\n";
+         "3. Modify a stock\n";
+         "4. Update stock prices\n";
+         "5. Return to financial menu\n";
+       ];
+     print_string [ Bold ] "Select an option: ";
+     stock_input user
 
-and prompt_add_stock user =
-  Lwt_io.printl "Enter stock symbol: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun symbol ->
-  Lwt_io.printl "Enter number of shares: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun shares ->
-  Lwt_io.printl "Enter purchase price: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun price ->
-  add_stock user symbol (int_of_string shares) (float_of_string price)
+   and stock_input user =
+     let choice = read_line () in
+     match choice with
+     | "1" -> prompt_add_stock user >>= fun () -> manage_stock_options user
+     | "2" -> prompt_remove_stock user >>= fun () -> manage_stock_options user
+     | "3" -> prompt_modify_stock user >>= fun () -> manage_stock_options user
+     | "4" -> update_stock_prices user >>= fun () -> manage_stock_options user
+     (* | "5" -> financial_interface user *)
+     | _ ->
+         Lwt_io.printl "Invalid choice. Please try again." >>= fun () ->
+         manage_stock_options user
 
-and prompt_remove_stock user =
-  Lwt_io.printl "Enter stock symbol to remove: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun symbol -> remove_stock user symbol
+   and prompt_add_stock user =
+     Lwt_io.printl "Enter stock symbol: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun symbol ->
+     Lwt_io.printl "Enter number of shares: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun shares ->
+     Lwt_io.printl "Enter purchase price: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun price ->
+     add_stock user symbol (int_of_string shares) (float_of_string price)
 
-and prompt_modify_stock user =
-  Lwt_io.printl "Enter stock index to modify: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun index ->
-  Lwt_io.printl "Enter new stock symbol: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun symbol ->
-  Lwt_io.printl "Enter new number of shares: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun shares ->
-  Lwt_io.printl "Enter new purchase price: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun price ->
-  Lwt_io.printl "Enter last known price: " >>= fun () ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun last_price ->
-  modify_stock user (int_of_string index) symbol (int_of_string shares)
-    (float_of_string price)
-    (float_of_string last_price)
+   and prompt_remove_stock user =
+     Lwt_io.printl "Enter stock symbol to remove: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun symbol -> remove_stock user symbol
+
+   and prompt_modify_stock user =
+     Lwt_io.printl "Enter stock index to modify: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun index ->
+     Lwt_io.printl "Enter new stock symbol: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun symbol ->
+     Lwt_io.printl "Enter new number of shares: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun shares ->
+     Lwt_io.printl "Enter new purchase price: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun price ->
+     Lwt_io.printl "Enter last known price: " >>= fun () ->
+     Lwt_io.read_line Lwt_io.stdin >>= fun last_price ->
+     modify_stock user (int_of_string index) symbol (int_of_string shares)
+       (float_of_string price)
+       (float_of_string last_price) *)
