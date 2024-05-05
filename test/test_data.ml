a2608e83169340ac517e0971f620ee30e0aa8819
Hannah Price <hep43@cornell.edu>
2024-05-05 12:42:19 -0400

    Test get_data

--- test/test_data.ml
+++ test/test_data.ml
@@ -11,6 +11,34 @@ let suite =
            assert_equal "\n -- \n"
              (get_data "data/for_testing/test1_quotes.csv" None)
              ~printer:(fun x -> x) );
+         ( "get_data empty limit" >:: fun _ ->
+           assert_equal "\n -- \n"
+             (get_data "data/for_testing/test1_quotes.csv" (Some 1))
+             ~printer:(fun x -> x) );
+         ( "get_data column no limit" >:: fun _ ->
+           assert_equal "\nquote 1\nquote 2\nquote 3\n"
+             (get_data "data/for_testing/test2_quotes.csv" None)
+             ~printer:(fun x -> x) );
+         ( "get_data column limit 1" >:: fun _ ->
+           assert_equal "\nquote 1\n"
+             (get_data "data/for_testing/test2_quotes.csv" (Some 1))
+             ~printer:(fun x -> x) );
+         ( "get_data column limit 2" >:: fun _ ->
+           assert_equal "\nquote 1\nquote 2\n"
+             (get_data "data/for_testing/test2_quotes.csv" (Some 2))
+             ~printer:(fun x -> x) );
+         ( "get_data column limit 0" >:: fun _ ->
+           assert_equal "\n\n"
+             (get_data "data/for_testing/test2_quotes.csv" (Some 0))
+             ~printer:(fun x -> x) );
+         ( "get_data rows no limit" >:: fun _ ->
+           assert_equal "\na b c\nd e f\ng h i\n"
+             (get_data "data/for_testing/test_data.csv" None) ~printer:(fun x ->
+               x) );
+         ( "get_data rows limit" >:: fun _ ->
+           assert_equal "\na b c\nd e f\n"
+             (get_data "data/for_testing/test_data.csv" (Some 2))
+             ~printer:(fun x -> x) );
        ]
 
 let () = run_tests suite
