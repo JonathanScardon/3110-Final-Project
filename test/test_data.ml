b95bdfd148d823aed6df4f5fc87970ae97633d22
Hannah Price <hep43@cornell.edu>
2024-05-05 12:55:37 -0400

    Test search

--- test/test_data.ml
+++ test/test_data.ml
@@ -3,6 +3,7 @@ open Test
 open Final_project.Data
 
 (* testing get_data, search, search2, find_entry, data_to_list *)
+(* test and add to .mli: remove_data_list, edit_data *)
 
 let suite =
   "Data.ml"
@@ -39,6 +40,34 @@ let suite =
            assert_equal "\na b c\nd e f\n"
              (get_data "data/for_testing/test_data.csv" (Some 2))
              ~printer:(fun x -> x) );
+         ( "get_data search first row" >:: fun _ ->
+           assert_equal true
+             (search "a" "data/for_testing/test_data.csv")
+             ~printer:string_of_bool );
+         ( "get_data search false but in csv" >:: fun _ ->
+           assert_equal false
+             (search "b" "data/for_testing/test_data.csv")
+             ~printer:string_of_bool );
+         ( "get_data search false not in csv" >:: fun _ ->
+           assert_equal false
+             (search "z" "data/for_testing/test_data.csv")
+             ~printer:string_of_bool );
+         ( "get_data search empty string" >:: fun _ ->
+           assert_equal false
+             (search "" "data/for_testing/test_data.csv")
+             ~printer:string_of_bool );
+         ( "get_data search second row" >:: fun _ ->
+           assert_equal true
+             (search "d" "data/for_testing/test_data.csv")
+             ~printer:string_of_bool );
+         ( "get_data search last row" >:: fun _ ->
+           assert_equal true
+             (search "g" "data/for_testing/test_data.csv")
+             ~printer:string_of_bool );
+         ( "get_data search duplicates" >:: fun _ ->
+           assert_equal true
+             (search "a" "data/for_testing/test2_data.csv")
+             ~printer:string_of_bool );
        ]
 
 let () = run_tests suite
