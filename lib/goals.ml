e9fb9105ea74533cd77dbbd355e8b8e0947448f1
Jonathan Scardon <js3294@cornell.edu>
2024-05-12 17:21:43 -0400

    Updated adding goals

--- lib/goals.ml
+++ lib/goals.ml
@@ -9,6 +9,13 @@ let add_new_goal user =
     print_string [Bold; Foreground Red] "Error: you've already added this goal!\n"
   else
     begin
-    Data.add_data (goal_name :: []) path;
-    print_string [Bold; Foreground Green] "Goal added successfully!\n"
+      Data.add_data [goal_name] path;
+      let goal_file_path = "data/" ^ user ^ "_" ^ goal_name ^ ".csv" in
+      try
+        Csv.save goal_file_path [];
+        print_string [Bold; Foreground Green] "Goal added successfully!\n"
+      with
+      | Sys_error msg ->
+        print_string [Foreground Red]
+          (Printf.sprintf "Failed to create file %s: %s\n" goal_file_path msg)
     end
