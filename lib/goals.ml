fbd57d2dc6d3e8d5244d3eb123ad29c52f769cc8
Jonathan Scardon <js3294@cornell.edu>
2024-05-13 16:47:24 -0400

    Modified add_goal to check completed goals

--- lib/goals.ml
+++ lib/goals.ml
@@ -13,12 +13,13 @@ let add_new_goal user =
   print_string [] "\n";
   print_string [] "What would you like to title your new goal?\n";
   let goal_name = read_line () in
-  let path = incomplete_goals_path user in
-  if Data.search goal_name path then
+  if Data.search goal_name (incomplete_goals_path user) then
     print_string [Bold; Foreground Red] "Error: you've already added this goal!\n"
+  else if Data.search goal_name (complete_goals_path user) then
+    print_string [Bold; Foreground Red] "Error: you've already completed this goal!\n"
   else
     begin
-      Data.add_data (goal_name :: Mood.curr_date :: []) path;
+      Data.add_data (goal_name :: Mood.curr_date :: []) (incomplete_goals_path user);
       let goal_file_path = "data/" ^ user ^ "_" ^ goal_name ^ ".csv" in
       try
         Csv.save goal_file_path []; (* Initialize the goal file with the goal name and date *)
