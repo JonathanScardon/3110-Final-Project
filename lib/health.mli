aa9c00e4a589b86ae9b044239aac588fe574f965
ashalis <abs325@cornell.edu>
2024-05-12 20:10:20 -0400

    Added a few specs to health

--- lib/health.mli
+++ lib/health.mli
@@ -1,8 +1,19 @@
 (* TODO: add specs *)
 
 val add_health_data : string -> string -> unit
+(** [add_health data user journal] prompts the user for input depending on 
+    which [journal] is provided, and adds user's health data to 
+    their journal. *)
+
 val search_entry : string -> unit
+(** [search_entry path] prompts the user to enter a journal to search and
+    a date in the format day-month-year (e.g., 2-3-2024) to search for entries 
+    in the CSV file at [path]. If 'back' is entered, the search is 
+    canceled. If the entry does not exist, it informs the user. *)
+
 val see_history : string -> unit
+(** [see_history path] allows users to view the data in the journal stored at [path]*)
+
 val select_journal : string -> (string -> unit) -> string -> unit
 val remove_entry : string -> unit
 
